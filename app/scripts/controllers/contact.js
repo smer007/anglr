c4805648ae715deaa5fb7277a5cd1586891dce1b
sumeer <sumeertuli99@yahoo.co.in>
2015-11-15 13:56:34 -0800

    adds a view to contact tab.

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anglrApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the anglrApp
+ */
+angular.module('anglrApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
