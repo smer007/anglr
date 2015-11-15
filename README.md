d37da0840fe91799455dd65d071ccce87d7bca2e
sumeer <sumeertuli99@yahoo.co.in>
2015-11-15 13:49:04 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# angularapp
+# anglr
 
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.14.0.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anglrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the anglrApp
+ */
+angular.module('anglrApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
