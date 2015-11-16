f8348c628c08095334bbe080424b842fdc8b3cd0
sumeer <sumeertuli99@yahoo.co.in>
2015-11-15 19:49:58 -0800

    adding employees and filtering them

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,28 @@
  */
 angular.module('anglrApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  	var mainVm = this;
+    mainVm.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    mainVm.Employees = [{
+    	"id": 1,
+    	"name": "Sumeer"
+    }, {
+    	"id": 2,
+    	"name": "Sam"
+    }, {
+    	"id": 3,
+    	"name": "Tuli"
+    }, {
+    	"id": 4,
+    	"name": "TuliSumeer"
+    }]
+
+    function addEmployee () {
+    	
+    }
+
   });
