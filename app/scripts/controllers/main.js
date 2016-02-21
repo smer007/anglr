9e36efb48114625ed9562c4d592d92173094cc98
Sumeer Tuli <sumeertuli7@gmail.com>
2016-02-21 00:23:53 -0800

    converts form into a table form and adds order by filter.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,34 +8,41 @@
  * Controller of the anglrApp
  */
 angular.module('anglrApp')
-  .controller('MainCtrl', function () {
-  	var mainVm = this;
+  .controller('MainCtrl', MainController);
+
+  function MainController() {
+    var mainVm = this;
     
     mainVm.Employees = [{
-    	"id": 1,
-    	"name": "Sumeer",
+      "id": 1,
+      "name": "Sumeer",
       "price": 4
     }, {
-    	"id": 2,
-    	"name": "Sam",
+      "id": 2,
+      "name": "Sam",
       "price": 4
     }, {
-    	"id": 3,
-    	"name": "Tuli",
+      "id": 3,
+      "name": "Tuli",
       "price": 4
     }, {
-    	"id": 4,
-    	"name": "TuliSumeer",
+      "id": 4,
+      "name": "TuliSumeer",
       "price": 4
     }]
 
     mainVm.addEmployee = function () {
-    	mainVm.Employees.push(mainVm.Employee);
-    	mainVm.Employee = null;
+      mainVm.Employees.push(mainVm.Employee);
+      mainVm.Employee = null;
     }
 
     mainVm.deleteEmployee = function (position) {
-    	mainVm.Employees.splice(position, 1);
+      mainVm.Employees.splice(position, 1);
     }
 
-  });
+    mainVm.sort = {
+      by: 'id',
+      reverse: false
+    }
+
+  }
